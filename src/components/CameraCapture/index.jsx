1d514e884f6f2a97593391306e49bdf038b7425d
fengfan <fengfan@xforceplus.com>
2024-09-17 22:51:23 +0800

    update

--- src/components/CameraCapture/index.jsx
+++ src/components/CameraCapture/index.jsx
@@ -67,10 +67,10 @@ const CameraCapture = ({backSrc}) => {
                 <Camera
                   onTakePhoto = { (dataUri) => { onTakePhoto(dataUri) } }
                   idealFacingMode = {FACING_MODES.ENVIRONMENT}
-                  isMaxResolution = {true}
-                  // idealResolution = {{width,height}}
+                  // isMaxResolution = {true}
+                  idealResolution = {{width,height}}
                   sizeFactor = {1}
-                  isFullscreen = {true}
+                  // isFullscreen = {true}
                 />
                 {/* <video  ref={videoRef} autoPlay playsInline onLoadedMetadata={handleLoadedMetadata}/> */}
                 {videoSrc ? <img className='img-bg'  src={videoSrc} />:
